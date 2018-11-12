b7f8d4a0b2be99614ba9ab1afe096b7d187279e4
xuxin <943512372@qq.com>
2018-11-12 14:42:40 +0800

    list

--- src/components/page-module/listbody.vue
+++ src/components/page-module/listbody.vue
@@ -1,6 +1,15 @@
 <template>
  <div class="listbody">
-
+     <div class="listtitle">
+        <ul class="sort-con">
+            <li class="sort-item active" data-type="default">默认排序</li>
+            <li class="sort-item" data-type="price">
+                <span>价格</span>
+                <i class="fa fa-sort-asc"></i>
+                <i class="fa fa-sort-desc"></i>
+            </li>
+        </ul>
+     </div>
  </div>
 </template>
 
@@ -18,5 +27,22 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+@import '@/assets/css/varibles.scss';
+.listbody{
+  width:100%;
+  .listtitle{
+    width:100%;
+    height:$headerHeight;
+    .sort-con{
+      width:100%;
+      height:100%;
+      overflow: hidden;
+      .sort-item{
+        width:50%;
+        height:100%;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
